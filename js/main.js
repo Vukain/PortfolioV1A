9268a429dc66fe23e72d37baa778a0a4b1b0d3ea
Michal Piechowiak <nilfenheim@gmail.com>
2020-07-24 00:17:29 +0200

    adjust color change

--- js/main.js
+++ js/main.js
@@ -7,8 +7,30 @@ if (sUsrAg.indexOf("Firefox") > -1) {
     })
 }
 
-const darkLight = document.querySelector(".fa-adjust")
+const queGreen = "images/que_green.svg";
+const queYellow = "images/que_yellow.svg";
+
+const queUno = document.querySelectorAll(".que_uno");
+const queDos = document.querySelectorAll(".que_dos");
+
+const darkLight = document.querySelector(".fa-adjust");
 
 darkLight.addEventListener("click", () => {
     document.body.classList.toggle("light");
+
+    if (document.body.className == "light") {
+        queUno.forEach(function (img) {
+            img.setAttribute("src", queYellow)
+        })
+        queDos.forEach(function (img) {
+            img.setAttribute("src", queGreen)
+        })
+    } else {
+        queUno.forEach(function (img) {
+            img.setAttribute("src", queGreen)
+        })
+        queDos.forEach(function (img) {
+            img.setAttribute("src", queYellow)
+        })
+    }
 })
